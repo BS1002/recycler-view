2fccae3b01593c5aa22c56a69d3a5f50229189ab
Md Mahfuzur Rahman <mahfuznow@gmail.com>
2021-11-30 21:03:51 +0600

    Updated CatAndFoodListAdapter Followed some coding conventions

--- app/src/main/java/com/mahfuznow/recyclerview/adapter/CatAndFoodListAdapter.kt
+++ app/src/main/java/com/mahfuznow/recyclerview/adapter/CatAndFoodListAdapter.kt
@@ -11,84 +11,90 @@ import com.mahfuznow.recyclerview.R
 import com.mahfuznow.recyclerview.model.Cat
 import com.mahfuznow.recyclerview.model.Food
 
-
-private const val VIEW_TYPE_CAT = 0
-private const val VIEW_TYPE_FOOD = 1
-private const val VIEW_TYPE_AD = 2
-
 class CatAndFoodListAdapter(private val context: Context, private val items: ArrayList<Any>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    companion object {
+        private const val VIEW_TYPE_CAT = 0
+        private const val VIEW_TYPE_FOOD = 1
+        private const val VIEW_TYPE_AD = 2
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        if (viewType == VIEW_TYPE_CAT)
-            return CatViewHolder(
+        return when (viewType) {
+            VIEW_TYPE_CAT -> CatViewHolder(
                 LayoutInflater.from(parent.context).inflate(R.layout.item_cat, parent, false)
             )
-        else if (viewType == VIEW_TYPE_FOOD)
-            return FoodViewHolder(
-                LayoutInflater.from(parent.context).inflate(R.layout.item_food, parent, false)
-            )
-        return AdViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_ad, parent, false)
-        )
+            VIEW_TYPE_FOOD ->
+                FoodViewHolder(
+                    LayoutInflater.from(parent.context).inflate(R.layout.item_food, parent, false)
+                )
+            else ->
+                AdViewHolder(
+                    LayoutInflater.from(parent.context).inflate(R.layout.item_ad, parent, false)
+                )
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = items[position]
-        val viewType = getItemViewType(position)
-        if (viewType == VIEW_TYPE_CAT) {
-            item as Cat
-            holder as CatViewHolder
-
-            holder.tvName.text = item.name
-            holder.tvAge.text = item.age.toString()
-            holder.tvOrigin.text = item.origin
-
-            holder.itemView.setOnClickListener {
-                Toast.makeText(context, "You have clicked ${item.name}", Toast.LENGTH_SHORT).show()
+        when (getItemViewType(position)) {
+            VIEW_TYPE_CAT -> {
+                item as Cat
+                holder as CatViewHolder
+                holder.onBind(context, item)
             }
-
-        } else if (viewType == VIEW_TYPE_FOOD) {
-            item as Food
-            holder as FoodViewHolder
-
-            holder.tvName.text = item.name
-            holder.tvPrice.text = item.price.toString()
-            holder.tvManufacturer.text = item.manufacturer
-
-            holder.itemView.setOnClickListener {
-                Toast.makeText(context, "You have clicked ${item.name}", Toast.LENGTH_SHORT).show()
+            VIEW_TYPE_FOOD -> {
+                item as Food
+                holder as FoodViewHolder
+                holder.onBind(context, item)
             }
-        } else {
-            TODO("Advertisement")
-            //Advertisement
+            else -> {
+                TODO("Advertisement")
+                //Advertisement
+            }
         }
     }
 
-    override fun getItemCount(): Int {
-        return items.size
-    }
+    override fun getItemCount() = items.size
 
     //this override is required to find out the viewType in onCreateViewHolder method
     override fun getItemViewType(position: Int): Int {
-        val item = items[position]
-        if (item is Cat)
-            return VIEW_TYPE_CAT
-        else if (item is Food)
-            return VIEW_TYPE_FOOD
-        return VIEW_TYPE_AD
+        return when (items[position]) {
+            is Cat -> VIEW_TYPE_CAT
+            is Food -> VIEW_TYPE_FOOD
+            else -> VIEW_TYPE_AD
+        }
     }
 
     class CatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvName: TextView = itemView.findViewById(R.id.tvName)
-        val tvAge: TextView = itemView.findViewById(R.id.tvAge)
-        val tvOrigin: TextView = itemView.findViewById(R.id.tvOrigin)
+        private val tvName: TextView = itemView.findViewById(R.id.tvName)
+        private val tvAge: TextView = itemView.findViewById(R.id.tvAge)
+        private val tvOrigin: TextView = itemView.findViewById(R.id.tvOrigin)
+
+        fun onBind(context: Context, item: Cat) {
+            tvName.text = item.name
+            tvAge.text = item.age.toString()
+            tvOrigin.text = item.origin
+            itemView.setOnClickListener {
+                Toast.makeText(context, "You have clicked ${item.name}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     class FoodViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvName: TextView = itemView.findViewById(R.id.tvName)
-        val tvPrice: TextView = itemView.findViewById(R.id.tvPrice)
-        val tvManufacturer: TextView = itemView.findViewById(R.id.tvManufacturer)
+        private val tvName: TextView = itemView.findViewById(R.id.tvName)
+        private val tvPrice: TextView = itemView.findViewById(R.id.tvPrice)
+        private val tvManufacturer: TextView = itemView.findViewById(R.id.tvManufacturer)
+
+        fun onBind(context: Context, item: Food) {
+            tvName.text = item.name
+            tvPrice.text = item.price.toString()
+            tvManufacturer.text = item.manufacturer
+            itemView.setOnClickListener {
+                Toast.makeText(context, "You have clicked ${item.name}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     class AdViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
